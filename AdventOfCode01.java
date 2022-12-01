37bd22dc3774b9c60cb4cb3deff49d08ab3b09ad
Jonas Koens <51293650+Jonas2k3@users.noreply.github.com>
2022-12-01 19:13:08 +0100

    Advent Of Code 2022, day 1
    Advent Of Code 2022, day 1 programmed in Java

---
+++ AdventOfCode01.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class AdventOfCode01{
+    public static void main(String[] args) throws FileNotFoundException{
+        File data = new File("AdventOfCode01Data.txt");
+        Scanner scan = new Scanner(data);
+        int max = 0;
+        int currentSum = 0;
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        ArrayList<Integer> calcValues = new ArrayList();
+        
+        while(scan.hasNext()){
+            String line = scan.nextLine();
+            if(line.isEmpty()){
+                for(int i = 0; i < temp.size(); i++){
+                    currentSum += temp.get(i);
+                }
+                System.out.println(currentSum);
+                calcValues.add(currentSum);
+                temp.clear();
+                currentSum = 0;
+            }
+            else if(!line.isEmpty()){
+                temp.add(Integer.parseInt(line));
+            }
+        }
+        Collections.sort(calcValues);
+        System.out.println(calcValues);
+        int sumOfNumbers = calcValues.get(calcValues.size()-1) + calcValues.get(calcValues.size()-2) + calcValues.get(calcValues.size()-3);
+        System.out.println(sumOfNumbers);
+    }
+}
