d9d3db4b08ae9f04370d1f0289844fb349643f0b
Jonas2k3 <jonaskoens2003@gmail.com>
2022-12-01 22:48:28 +0100

    Refactor to display answers for both parts clearer

--- AdventOfCode01.java
+++ AdventOfCode01.java
@@ -8,7 +8,6 @@ public class AdventOfCode01{
     public static void main(String[] args) throws FileNotFoundException{
         File data = new File("AdventOfCode01Data.txt");
         Scanner scan = new Scanner(data);
-        int max = 0;
         int currentSum = 0;
         ArrayList<Integer> temp = new ArrayList<>(); 
         ArrayList<Integer> calcValues = new ArrayList();
@@ -29,6 +28,7 @@ public class AdventOfCode01{
         }
         Collections.sort(calcValues);
         int sumOfNumbers = calcValues.get(calcValues.size()-1) + calcValues.get(calcValues.size()-2) + calcValues.get(calcValues.size()-3);
-        System.out.println(sumOfNumbers);
+        System.out.println("Part one answer: " + calcValues.get(calcValues.size() - 1));
+        System.out.println("Part two answer: " + sumOfNumbers);
     }
 }
